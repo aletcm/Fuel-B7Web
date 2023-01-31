b5d76b821077ee1975540f7ff996f3e3d497ae36
Alessandro Taddei <alessandro.taddei@outlook.com>
2023-01-31 12:32:26 -0300

    First commit for this project (fuel calculator)

---
+++ README.txt
@@ -0,0 +1,2 @@
+Este é um exercício de desafio proposto pela plataforma B7Web: Calculadora de comparação de preço de combustíveis 
+em HTML-CSS e JavaScript.
---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+/* JS code requested by script.html file; Javascript content to HTML code */
+
+function formHandler(received) {
+    received.preventDefault()
+
+    let [getGasoline, getAlcohol] = received.target
+    
+    let setGasoline = getGasoline.value.replace(',', '.')
+    let setAlcohol = getAlcohol.value.replace(',', '.')
+
+    if(setAlcohol / setGasoline > 0.7) {
+        document.getElementsByTagName('div')[3].classList.add('winner')
+        showMessage('GASOLINA')
+    } else {
+        document.getElementsByTagName('div')[4].classList.add('winner')
+        showMessage('ÁLCOOL')
+    }
+}
+
+function showMessage(fuel) {
+    document.getElementById('messages').innerHTML = "É mais vantajoso abastecer com "+fuel
+}
+
+function clearMessages() {
+    let inputs = document.querySelectorAll('input')
+
+    for(let i = 0; i < inputs.length; i++) {
+        inputs[i].value = ""
+    }
+
+    document.getElementById('messages').innerHTML = ""
+    document.querySelectorAll('.winner').forEach(element => element.classList.remove('winner'))
+}
